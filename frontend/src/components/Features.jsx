850ecd30c032344e20ae86115970674afafae27d
TylerRake7468 <lalitpalariya450@gmail.com>
2024-10-01 23:57:52 +0530

    added dummy features

--- frontend/src/components/Features.jsx
+++ frontend/src/components/Features.jsx
@@ -1,9 +1,65 @@
+const features = [
+    {
+        title: 'Monthly Code Challenge',
+        description: 'Engage in monthly code challenges to enhance your skills.',
+        image: <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M14.25 9.75 16.5 12l-2.25 2.25m-4.5 0L7.5 12l2.25-2.25M6 20.25h12A2.25 2.25 0 0 0 20.25 18V6A2.25 2.25 0 0 0 18 3.75H6A2.25 2.25 0 0 0 3.75 6v12A2.25 2.25 0 0 0 6 20.25Z" />
+                </svg>
+    },
+    {
+        title: 'Monthly Code Challenge',
+        description: 'Engage in monthly code challenges to enhance your skills.',
+        image: <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M14.25 9.75 16.5 12l-2.25 2.25m-4.5 0L7.5 12l2.25-2.25M6 20.25h12A2.25 2.25 0 0 0 20.25 18V6A2.25 2.25 0 0 0 18 3.75H6A2.25 2.25 0 0 0 3.75 6v12A2.25 2.25 0 0 0 6 20.25Z" />
+                </svg>
+    },
+    {
+        title: 'Monthly Code Challenge',
+        description: 'Engage in monthly code challenges to enhance your skills.',
+        image: <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M14.25 9.75 16.5 12l-2.25 2.25m-4.5 0L7.5 12l2.25-2.25M6 20.25h12A2.25 2.25 0 0 0 20.25 18V6A2.25 2.25 0 0 0 18 3.75H6A2.25 2.25 0 0 0 3.75 6v12A2.25 2.25 0 0 0 6 20.25Z" />
+                </svg>
+    },
+    {
+        title: 'Monthly Code Challenge',
+        description: 'Engage in monthly code challenges to enhance your skills.',
+        image: <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M14.25 9.75 16.5 12l-2.25 2.25m-4.5 0L7.5 12l2.25-2.25M6 20.25h12A2.25 2.25 0 0 0 20.25 18V6A2.25 2.25 0 0 0 18 3.75H6A2.25 2.25 0 0 0 3.75 6v12A2.25 2.25 0 0 0 6 20.25Z" />
+                </svg>
+    },
+    {
+        title: 'Monthly Code Challenge',
+        description: 'Engage in monthly code challenges to enhance your skills.',
+        image: <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M14.25 9.75 16.5 12l-2.25 2.25m-4.5 0L7.5 12l2.25-2.25M6 20.25h12A2.25 2.25 0 0 0 20.25 18V6A2.25 2.25 0 0 0 18 3.75H6A2.25 2.25 0 0 0 3.75 6v12A2.25 2.25 0 0 0 6 20.25Z" />
+                </svg>
+    },
+    {
+        title: 'Monthly Code Challenge',
+        description: 'Engage in monthly code challenges to enhance your skills.',
+        image: <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M14.25 9.75 16.5 12l-2.25 2.25m-4.5 0L7.5 12l2.25-2.25M6 20.25h12A2.25 2.25 0 0 0 20.25 18V6A2.25 2.25 0 0 0 18 3.75H6A2.25 2.25 0 0 0 3.75 6v12A2.25 2.25 0 0 0 6 20.25Z" />
+                </svg>
+    }
+]
 const Features = () =>{
     return (
         <>
             <div className="bg-white">
                 <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
                     <h3 className="text-xl font-bold">Incentive</h3>
+
+                    { 
+                        features.map((feature, index) => {
+                            return (
+                                <div key={index}>
+                                    {feature.image}
+                                    <p>{feature.title}</p>
+                                    <p>{feature.description}</p>
+                                </div>
+                            );
+                        }) 
+                    }
                 </div>
             </div>
         </>
