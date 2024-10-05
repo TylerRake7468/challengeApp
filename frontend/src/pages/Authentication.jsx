45f2d9528a79a959046fdbf24b5070f666752a47
TylerRake7468 <lalitpalariya450@gmail.com>
2024-10-05 23:50:06 +0530

    added validation features for login signup

--- frontend/src/pages/Authentication.jsx
+++ frontend/src/pages/Authentication.jsx
@@ -1,18 +1,36 @@
 import PropType from 'prop-types'
 import { useState } from 'react';
+import { validateEmail, validatePassword } from '../utilities/validations';
 
 const Authentication = ({pageType}) =>{
+    const initialErrorsState = {
+        email: '',
+        password: '',
+        api: ''
+    }
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [errors, setErrors] = useState(initialErrorsState);
 
     const handleEmailChange = (e) => {
         setEmail(e.target.value);
         console.log(email);
     }
 
+ 
     const handlePasswordChange = (e) => {
         setPassword(e.target.value);
         console.log(password);
+    }
+
+    const handleSubmit = (e) =>{
+        if(!validateEmail(email)){
+
+        }
+        if(!validatePassword(password)){
+
+        }
+        // make api call if success
     }
 
     return (
@@ -25,24 +43,30 @@ const Authentication = ({pageType}) =>{
                     }
                     </h3>
 
-                    <form className='mt-10 max-w-64 flex flex-col gap-2'>
-                        <input 
-                            name="email" 
-                            type='email' 
-                            placeholder='Enter email' 
-                            className='py-2 border border-gray-600 rounded px-3'
-                            onChange={handleEmailChange}
-                            value={email}
-                        />
+                    <form onSubmit={handleSubmit} className='mt-10 max-w-64 flex flex-col gap-6'>
+                        <div>
+                            <input 
+                                name="email" 
+                                type='email' 
+                                placeholder='Enter email' 
+                                className='py-2 border border-gray-600 rounded px-3'
+                                onChange={handleEmailChange}
+                                value={email}
+                            />
+                            { errors.email && <p className='text-sm text-red-500 text-medium'>{errors.email}</p> }
+                        </div>
 
-                        <input 
-                            name="password" 
-                            type='password' 
-                            placeholder='Enter password' 
-                            className='py-2 border border-gray-600 rounded px-3'
-                            onChange={handlePasswordChange}
-                            value={password}
-                        />
+                        <div>
+                            <input 
+                                name="password" 
+                                type='password' 
+                                placeholder='Enter password' 
+                                className='py-2 border border-gray-600 rounded px-3'
+                                onChange={handlePasswordChange}
+                                value={password}
+                            />
+                            { errors.password && <p className='text-sm text-red-500 text-medium'>{errors.password}</p> }
+                        </div>
                         <button type='submit' className='bg-indigo-500 rounded px-3 py-2 hover:bg-indigo-600 text-white'>
                             {(pageType === PageType.LOGIN) ? 'Login' : 'Sign Up'} 
                         </button>
