6798ef6b893ad0412ad2f7fcd6e292d2ba06d2ff
TylerRake7468 <lalitpalariya450@gmail.com>
2024-10-07 20:06:21 +0530

    created login signup routes and api call to rails

--- backend/app/controllers/users/registrations_controller.rb
+++ backend/app/controllers/users/registrations_controller.rb
@@ -3,21 +3,23 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   include RackSessionFix
   respond_to :json
+
   private
 
   def respond_with(resource, _opts = {})
     if request.method == "POST" && resource.persisted?
       render json: {
-        status: {message: "Signed up sucessfully."},
+        message: "Signed up sucessfully.",
         data: resource
       }, status: :ok
     elsif request.method == "DELETE"
       render json: {
-        status: {message: "Account deleted successfully."}
+        message: "Account deleted successfully."
       }, status: :ok
     else
       render json: {
-        status: {message: "User couldn't be created successfully.", errors: resource.errors.full_messages.to_sentence}
+        message: "User couldn't be created successfully.",
+        errors: resource.errors.full_messages.to_sentence
       }, status: :unprocessable_entity
     end
   end
