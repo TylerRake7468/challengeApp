d262a8ae3d8196b0c2134fc3e745274af783342d
TylerRake7468 <lalitpalariya450@gmail.com>
2024-11-11 21:49:18 +0530

    added react quill reach text completed frontend part

--- frontend/src/pages/AddChallenge.jsx
+++ frontend/src/pages/AddChallenge.jsx
@@ -9,42 +9,72 @@ const AddChallenge = () =>{
     const MIN_DATE = new Date();
     const [title, setTitle] = useState('')
     const [description, setDescription] = useState('')
+    const initialErrorsState = {
+        title: '',
+        description: '',
+        date: '',
+        api:''
+    }
+    const [errors, setErrors] = useState(initialErrorsState);
     const [value, setValue] = useState({ 
         startDate: null, 
         endDate: null
     });
     const handleTitleChange = (e) =>{
+        console.log("Title::::", e.target.value)
         setTitle(e.target.value)
     }
 
     const handleChangeDescription = (e) =>{
-        setTitle(e.target.value)
+        console.log("Desc::::", e)
+        setDescription(e)
     }
     const handleSubmit = (e) =>{
-        e.preventDefault()
+        e.preventDefault();
+        let newErrors = {};
+
+        if(title.length === 0){
+            newErrors = {
+                ...newErrors,
+                title: "Blank title"
+            };
+        }
+        if(description.length === 0){
+            newErrors = {
+                ...newErrors,
+                description: "Blank description"
+            };
+        }
+        if(value.startDate === null || value.endDate === null){
+            newErrors = {
+                ...newErrors,
+                date: "Blank start/end date."
+            };  
+        }
+        setErrors(newErrors);
+        const hasErrors = Object.values(newErrors).some(error=> error!='');
+        if(hasErrors){
+            return;
+        }
         console.log("form submitted")
+        // TODO: Api call
     }
     return (
         <>
             <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8 py-12">
                 <h1 className="text-4xl">Add Challenge</h1>
                 <form onSubmit={handleSubmit} className='mt-10 flex flex-col gap-6'>
-                    <input 
-                        name="title" 
-                        type='title' 
-                        placeholder='Enter challenge title' 
-                        className='py-2 w-full border border-gray-600 rounded px-3'
-                        onChange={handleTitleChange}
-                        value={title}
-                    />
-                    <Datepicker
-                        minDate={MIN_DATE}
-                        inputClassName="py-2 w-full border border-gray-600 rounded px-3"
-                        popoverDirection="down"
-                        value={value} 
-                        onChange={newValue => setValue(newValue)}
-                    />
-                    <ReactQuill theme="snow" value={description} onChange={handleChangeDescription} />
+                    <div>
+                        <input 
+                            name="title" 
+                            type='title' 
+                            placeholder='Enter challenge title' 
+                            className='py-2 w-full border border-gray-600 rounded px-3'
+                            onChange={handleTitleChange}
+                            value={title}
+                        />
+                        { errors.title && <p className='text-sm text-red-500 text-medium'>{errors.title}</p> }
+                    </div>
                     <div className="text-editor">
                         <EditorToolbar />
                         <ReactQuill
@@ -55,8 +85,22 @@ const AddChallenge = () =>{
                             modules={modules}
                             formats={formats}
                         />
+                        { errors.description && <p className='text-sm text-red-500 text-medium'>{errors.description}</p> }
                     </div>
-                    <Button type="submit">Add Challenge</Button>
+                    <div>
+                        <Datepicker
+                            minDate={MIN_DATE}
+                            inputClassName="py-2 w-full border border-gray-600 rounded px-3"
+                            popoverDirection="down"
+                            value={value} 
+                            onChange={newValue => setValue(newValue)}
+                        />
+                        { errors.date && <p className='text-sm text-red-500 text-medium'>{errors.date}</p> }
+                    </div>
+                    <div>
+                        <Button type="submit">Add Challenge</Button>
+                        { errors.api && <p className='text-sm text-red-500 text-medium'>{errors.api}</p> }
+                    </div>
                 </form>
 
             </div>
